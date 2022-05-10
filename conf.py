bf53a0ecf61f094193d0b36324a211c957c03284
Carlos55150233 <102787946+Carlos55150233@users.noreply.github.com>
2022-05-10 16:27:02 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "e2f08a01a16c7e3c4805e8fae8a59f1f"
-api_id = 8293133
-bot_token = "5216365411:AAELcVXgF6j2LRNoanmcyAFG561p0d_Oeeo"
-bot_admin_group = -1001306761856
+api_hash = "382ee6b53bdd0df66a52ea9779c62424"
+api_id = 18693993
+bot_token = "5321111496:AAEgR3W3mBKGsijPB9ePS1pZTNzIPyfpLxs"
+bot_admin_group = -1001637160750
