d32504ce8765e1e101c23ba8dff492c462166397
Carlos55150233 <102787946+Carlos55150233@users.noreply.github.com>
2022-05-14 00:58:18 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "382ee6b53bdd0df66a52ea9779c62424"
 api_id = 18693993
-bot_token = "5321111496:AAEgR3W3mBKGsijPB9ePS1pZTNzIPyfpLxs"
-bot_admin_group = -1001637160750
+bot_token = "5395130445:AAHRKMGuURKlBPEE2D4gySOc7UGRv-69xBw"
+bot_admin_group = -1001556539806
